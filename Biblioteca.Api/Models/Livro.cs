0bb4211f2a26e5fb8fde80b0680aa210f7fa9c93
Cervelati <luizhenriquesilvacervelati@gmail.com>
2026-07-19 09:57:37 -0300

    feat: add Livro in models

---
+++ Biblioteca.Api/Models/Livro.cs
@@ -0,0 +1,11 @@
+
+
+public class Livro  {
+
+    public int Id {get; set;}
+    public string Titulo {get; set;}
+    public string Autor {get; set;}
+    public string Isbn {get; set;}
+    public DateTime AnoPublicacao {get; set;}
+    public int Estoque {get; set;}
+}
